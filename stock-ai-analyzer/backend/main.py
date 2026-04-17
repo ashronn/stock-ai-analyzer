97df4e5def0628a31663064d43fa965b0a365daf
ashronn <ash.ashron@gmail.com>
2026-04-18 02:08:50 +0700

    Fix CORS: allow all origins

--- stock-ai-analyzer/backend/main.py
+++ stock-ai-analyzer/backend/main.py
@@ -16,8 +16,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://stock-ai-analyzer-jet.vercel.app/", # วาง URL ที่ก๊อปมา (ลบ / ตัวสุดท้ายออกด้วยนะครับ)
-        "http://localhost:5500"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
